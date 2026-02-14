2ce2a31eaa38ac9897a043695a59c2c2ff0e1624
arispretz <arispretz@gmail.com>
2026-02-14 20:36:55 +0000

    feat: add autocomplete route

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ import { userProfileRouter } from './routes/userProfileRoutes.js';
 import { avatarRouter } from './routes/avatarRoutes.js';
 import { securityMiddleware } from './security.js';
 
+import { autocompleteRouter } from './routes/autocompleteRoutes.js';
 import { globalErrorHandler } from './middleware/errorHandlers.js';
 import { registerRouter } from './routes/registerRoutes.js';
 
@@ -116,6 +117,7 @@ app.use('/api', auth);
  * API routes
  */
 app.use('/api', registerRouter); 
+app.use('/api', autocompleteRouter); 
 app.use('/api', userProfileRouter);
 app.use('/api/code', codeRouter);
 app.use('/api', lintRouter);
